3d4678632552174b052c542b48cc93104a3144a0
goviioo <govind.ve@ioolabs.com>
2020-08-03 15:17:07 +0530

    Added constants

---
+++ constant.js
@@ -0,0 +1,42 @@
+var languagesConstants = {
+    java: {
+        name: 'Java',
+        url: 'https://img.icons8.com/dusk/96/000000/java-coffee-cup-logo.png'
+    },
+    html: {
+        name: 'HTML',
+        url: 'https://img.icons8.com/nolan/64/html.png'
+    },
+    css: {
+        name: 'CSS',
+        url: 'https://img.icons8.com/office/96/000000/css-filetype.png'
+    },
+    javascript: {
+        name: 'JavaScript',
+        url: 'https://img.icons8.com/dusk/96/000000/javascript.png'
+    },
+    reactnative: {
+        name: 'ReactNative',
+        url: 'https://img.icons8.com/nolan/96/react-native.png'
+    },
+    react: {
+        name: 'React',
+        url: 'https://img.icons8.com/plasticine/100/000000/react.png'
+    },
+    angular: {
+        name: 'Angular',
+        url: 'https://img.icons8.com/color/96/000000/angularjs.png'
+    },
+    vue: {
+        name: 'Vue',
+        url: 'https://img.icons8.com/color/96/000000/vue-js.png'
+    },
+    flutter: {
+        name: 'Flutter',
+        url: 'https://img.icons8.com/color/96/000000/flutter.png'
+    },
+    nodejs: {
+        name: 'Node js',
+        url: 'https://img.icons8.com/color/96/000000/nodejs.png'
+    }
+}
